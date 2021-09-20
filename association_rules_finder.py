62fce6e340457caa0ee3117cc6351ef315402a82
akalanka sakalasooriya <sakalasooriyaakalanka@gmail.com>
2021-09-20 11:30:10 +0530

    algo fix

--- association_rules_finder.py
+++ association_rules_finder.py
@@ -6,12 +6,13 @@ def get_rules(movi_list):
     data = pd.read_pickle(os.getcwd()+"/models/association_rules/rules.pkl")
     retvalue = []
     while len(movi_list) > 1 and len(retvalue) <= 0:
-        del movi_list[0]
         retvalue = []
         for index, row in data.iterrows():
             if row['antecedents'] == movi_list:
                 retvalue.extend(row["consequents"])
         if(len(retvalue) > 0):
             break
+        else:
+            del movi_list[0]
     retvalue = np.unique(retvalue)
     return list(retvalue)
