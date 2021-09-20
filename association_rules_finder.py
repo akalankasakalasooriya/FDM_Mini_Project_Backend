be49b59a4f9cc8007719ee74f1296b0d85b02144
akalanka sakalasooriya <sakalasooriyaakalanka@gmail.com>
2021-09-20 07:59:31 +0530

    pkl added

---
+++ association_rules_finder.py
@@ -0,0 +1,17 @@
+import os
+import numpy as np
+import pandas as pd
+
+def get_rules(movi_list):
+    data = pd.read_pickle(os.getcwd()+"/models/association_rules/rules.pkl")
+    retvalue = []
+    while len(movi_list) > 1 and len(retvalue) <= 0:
+        del movi_list[0]
+        retvalue = []
+        for index, row in data.iterrows():
+            if row['antecedents'] == movi_list:
+                retvalue.extend(row["consequents"])
+        if(len(retvalue) > 0):
+            break
+    retvalue = np.unique(retvalue)
+    return list(retvalue)
