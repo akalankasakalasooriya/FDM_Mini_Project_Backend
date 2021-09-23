0d33d68cbdf54e43cec782ccaab8e75fb30f4098
thisisishara <thisismaduishara@gmail.com>
2021-09-23 07:14:10 +0530

    model refinements

--- app.py
+++ app.py
@@ -1,16 +1,30 @@
 from flask import Flask, render_template, jsonify, request
+import association_rules_finder
 import classification_logistic_reg_model as lrm
 import classification_linear_svm_model as lsm
 import classification_random_forest_model as rfm
 import classification_perceptron_model as pm
 import classification_decision_tr_model as dtr
+import re
+
 
 app = Flask(__name__)
 
 
 @app.route("/MLService/association", methods=['GET', 'POST'])
 def associationRuleMine():
-    pass
+    req_data = request.json
+    result = []
+
+    movi_list = [int(x) for x in re.sub('"', "", str(
+        req_data["MOVI_LIST"]).strip()).split(",")]
+    print(movi_list)
+
+    if req_data["ALGO"] == 'FPG':
+        result = association_rules_finder.get_rules_fp(movi_list)
+    elif req_data["ALGO"] == 'APR':
+        result = association_rules_finder.get_rules_ap(movi_list)
+    return (jsonify({"MOVIES": str(result)}))
 
 
 @app.route("/MLService/classification", methods=['GET', 'POST'])
